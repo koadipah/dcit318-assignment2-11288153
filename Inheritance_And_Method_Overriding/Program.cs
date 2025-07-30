6ea11d0642b6d29749f52a3687915293213971aa
koadipah <kaosei023@st.ug.edu.gh>
2025-07-30 20:40:33 +0000

    added inheritance and methods

---
+++ Inheritance_And_Method_Overriding/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; }
+    public double Height { get; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class AbstractClassDemo
+{
+    static void Main()
+    {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine($"Circle area: {circle.GetArea():F2}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea():F2}");
+    }
+}
